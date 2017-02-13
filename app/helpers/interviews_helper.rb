ab9609a732606a668ab1f3436a4b961ab0d6f49a
Daiki Matoba <telnetstat@gmail.com>
2017-02-13 22:41:06 +0900

    Make views responsive

--- app/helpers/interviews_helper.rb
+++ app/helpers/interviews_helper.rb
@@ -1,2 +1,6 @@
 module InterviewsHelper
+  def category_label(category)
+    class_name = category == '面接' ? 'formal' : 'casual'
+    "<span class='label #{class_name}'>#{category}</span>".html_safe
+  end
 end
