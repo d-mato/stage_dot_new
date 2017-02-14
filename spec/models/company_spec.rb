c3f9385fbc3e76e75cbe76a22e95e0a0ce774409
Daiki Matoba <telnetstat@gmail.com>
2017-02-14 11:22:19 +0900

    Update model test

--- spec/models/company_spec.rb
+++ spec/models/company_spec.rb
@@ -1,9 +1,18 @@
 require 'rails_helper'
 
 describe Company do
+  let(:user) { User.create(email: "#{SecureRandom.hex}@example.com") }
+  let(:company) { Company.new }
+
   it 'is invalid when name is blank' do
-    company = Company.new(name: '')
+    company.attributes = { name: '', user: user }
     expect(company).not_to be_valid
     expect(company.errors[:name]).to be_present
+  end
+
+  it 'is valid when name and user_id are present' do
+    company.attributes = { name: 'John', user: user }
+    expect(company).to be_valid
+    expect(company.errors.size).to eq 0
   end
 end
