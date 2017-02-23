362c4079b7241d439de463bc2c95c26e5d003bf2
Daiki Matoba <telnetstat@gmail.com>
2017-02-24 00:44:56 +0900

    Create view 'settings/account'

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -6,8 +6,10 @@ class SettingsController < ApplicationController
 
     current_user.name = params[:name] if params[:name].present?
     current_user.email = params[:email] if params[:email].present?
-    current_user.save
 
+    return render unless current_user.save
+
+    flash[:success] = 'Account was successfully updated.'
     redirect_to settings_account_path
   end
 end
