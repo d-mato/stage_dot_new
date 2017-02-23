383a6d08a98c336471c5881cf465c6f31be28c86
Daiki Matoba <telnetstat@gmail.com>
2017-02-23 23:00:32 +0900

    Enable to sign in easily on dev

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  before_action :sign_in_for_dev, if: -> { Rails.env.development? && params[:user_id] }
+
+  private
+
+  # 開発環境用の自動ログイン機能
+  def sign_in_for_dev
+    sign_in User.find(params[:user_id])
+  end
 end
