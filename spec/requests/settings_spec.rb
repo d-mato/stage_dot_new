20320456fc33f08e6be5a7824ade5c35d13689ab
Daiki Matoba <telnetstat@gmail.com>
2017-02-24 00:56:04 +0900

    Add test for settings/account

--- spec/requests/settings_spec.rb
+++ spec/requests/settings_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 describe 'Settings' do
-  let(:user) { FactoryGirl.create :user, name: Faker::Name.name }
+  let!(:user) { FactoryGirl.create :user, name: Faker::Name.name }
   before { sign_in user }
 
   describe 'GET /settings/account' do
@@ -10,12 +10,35 @@ describe 'Settings' do
   end
 
   describe 'PUT /settings/account' do
-    let(:new_name) { Faker::Name.name }
-    let(:new_email) { 'new-email@example.com' }
-    before { put '/settings/account', params: { name: new_name, email: new_email } }
+    context 'invalid values' do
+      let!(:defaults) { { name: user.name, email: user.email } }
 
-    it('user.name was updated') { expect(user.reload.name).to eq new_name }
-    it('user.email was updated') { expect(user.reload.email).to eq new_email }
-    it { expect(response).to redirect_to('/settings/account') }
+      it '名前が空なら更新しない' do
+        put '/settings/account', params: { name: '', email: Faker::Internet.email }
+        expect(user.reload.name).to eq defaults[:name]
+      end
+
+      it 'Emailが空なら更新しない' do
+        put '/settings/account', params: { name: Faker::Name.name, email: '' }
+        expect(user.reload.email).to eq defaults[:email]
+      end
+
+      it '既に使用されているEmailを入力したら更新せずにエラーメッセージを表示すること' do
+        someone = FactoryGirl.create :user, email: 'someone@example.com'
+        put '/settings/account', params: { email: someone.email }
+        expect(user.reload.email).to eq defaults[:email]
+        expect(response.body).to include 'メールアドレスはすでに存在します'
+      end
+    end
+
+    context 'valid values' do
+      let(:new_name) { Faker::Name.name }
+      let(:new_email) { 'new-email@example.com' }
+      before { put '/settings/account', params: { name: new_name, email: new_email } }
+
+      it('user.name was updated') { expect(user.reload.name).to eq new_name }
+      it('user.email was updated') { expect(user.reload.email).to eq new_email }
+      it { expect(response).to redirect_to('/settings/account') }
+    end
   end
 end
