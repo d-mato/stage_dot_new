2b833862286c80e091c939df086c9c8a0b766087
Daiki Matoba <telnetstat@gmail.com>
2017-03-25 11:36:24 +0900

    Fix a bug

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -1,7 +1,7 @@
 class FeedsController < ApplicationController
   before_action :authenticate_user!
   def index
-    users = current_user.friends.pluck(:id) + [current_user]
+    users = current_user.friends + [current_user]
     @feeds = Version.of_users(users).order(created_at: :desc)
   end
 end
