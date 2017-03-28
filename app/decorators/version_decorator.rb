fba40813d3089e745927091bcf33327483cd5d80
Daiki Matoba <telnetstat@gmail.com>
2017-03-29 00:06:52 +0900

    Update feed message to change with version's event field

--- app/decorators/version_decorator.rb
+++ app/decorators/version_decorator.rb
@@ -2,11 +2,22 @@ module VersionDecorator
   def target_link
     case item
     when Company
-      link_to "#{item.name}の企業情報", user_company_path(item.user, item)
+      link_to item.name, user_company_path(item.user, item)
     when Interview
-      link_to "#{item.company.name}の面談情報", user_interview_path(item.user, item)
+      link_to "#{item.company.name}の#{item.category}", user_interview_path(item.user, item)
     when Resume
       link_to 'レジュメ', user_path(item.user)
+    end
+  end
+
+  def event_label
+    case event
+    when 'create'
+      '作成'
+    when 'update'
+      '更新'
+    else
+      '更新'
     end
   end
 end
--- spec/decorators/version_decorator_spec.rb
+++ spec/decorators/version_decorator_spec.rb
@@ -4,4 +4,22 @@ describe VersionDecorator do
   let(:version) { Version.new.extend VersionDecorator }
   subject { version }
   it { should be_a Version }
+
+  describe 'event_label' do
+    subject { version.event_label }
+    it 'createなら「作成」' do
+      version.event = 'create'
+      is_expected.to eq '作成'
+    end
+
+    it 'updateなら「更新」' do
+      version.event = 'update'
+      is_expected.to eq '更新'
+    end
+
+    it 'それ以外なら「更新」' do
+      version.event = 'other'
+      is_expected.to eq '更新'
+    end
+  end
 end
