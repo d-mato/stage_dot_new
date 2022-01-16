189bf6493d51260de4cfab7394b929cb238340f5
Daiki Matoba <telnetstat@gmail.com>
2022-01-16 09:03:05 +0900

    Fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,30 +1,27 @@
 Rails.application.routes.draw do
-  resources :companies, shallow: true do
-    get :archives, on: :collection
-    match :archive, via: [:post, :delete]
-    resources :interviews, except: [:index]
-  end
-  resources :interviews, only: [:index]
-  resource :resume, only: [:show, :edit, :update]
-  match 'settings/account', via: [:get, :put]
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
 
   resource :contact, only: [:show, :create]
 
-  resources :users, only: [:index, :show] do
-    resources :companies, only: [:show], controller: 'users/companies'
-    resources :interviews, only: [:show], controller: 'users/interviews'
-  end
-
-  resources :feeds, only: [:index]
-
   authenticated :user do
     root 'home#index'
+    resources :companies, shallow: true do
+      get :archives, on: :collection
+      match :archive, via: [:post, :delete]
+      resources :interviews, except: [:index]
+    end
+    resources :interviews, only: [:index]
+    resource :resume, only: [:show, :edit, :update]
+    match 'settings/account', via: [:get, :put]
+    resources :users, only: [:index, :show] do
+      resources :companies, only: [:show], controller: 'users/companies'
+      resources :interviews, only: [:show], controller: 'users/interviews'
+    end
+    resources :feeds, only: [:index]
   end
 
   unauthenticated :user do
     root 'static_pages#index'
   end
-
-  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
-  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
 end
