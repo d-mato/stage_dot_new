6630ca1e45ec62053679e5dc6043b410619942ce
Daiki Matoba <telnetstat@gmail.com>
2022-01-16 14:26:20 +0900

    Upgrade Rails to 6.1.4.4

--- config/application.rb
+++ config/application.rb
@@ -9,12 +9,17 @@ Bundler.require(*Rails.groups)
 module StageDotNew
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.1
+    config.load_defaults 6.1
 
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+    # Configuration for the application, engines, and railties goes here.
+    #
+    # These settings can be overridden in specific environments using the files
+    # in config/environments, which are processed later.
+    #
+    # config.time_zone = "Central Time (US & Canada)"
+    # config.eager_load_paths << Rails.root.join("extras")
+
     config.time_zone = 'Tokyo'
-    config.autoload_paths << Rails.root.join('lib')
+    config.eager_load_paths << Rails.root.join('lib')
   end
 end
